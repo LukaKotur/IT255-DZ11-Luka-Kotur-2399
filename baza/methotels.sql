71f02e41531b8673d7c176735577d18908a61e03
LukaKotur <looko95@gmail.com>
2016-06-13 18:04:36 +0200

    Changed Database

--- baza/methotels.sql
+++ baza/methotels.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 13, 2016 at 04:57 PM
+-- Generation Time: Jun 13, 2016 at 06:04 PM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 5.6.19
 
@@ -19,6 +19,24 @@ SET time_zone = "+00:00";
 --
 -- Database: `methotels`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotel`
+--
+
+CREATE TABLE `hotel` (
+  `id` int(11) NOT NULL,
+  `ime_hotela` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hotel`
+--
+
+INSERT INTO `hotel` (`id`, `ime_hotela`) VALUES
+(2, 'Hotel 2');
 
 -- --------------------------------------------------------
 
@@ -41,7 +59,9 @@ INSERT INTO `sobe` (`id`, `ime_sobe`, `broj_kvadrata`, `broj_kreveta`) VALUES
 (1, 'Soba 1', 20, 3),
 (2, 'Soba 2 ', 30, 4),
 (3, 'Soba 3', 40, 2),
-(4, 'Soba 4', 40, 3);
+(4, 'Soba 4', 40, 3),
+(5, 'Soba 5', 40, 4),
+(6, 'Soba 6 ', 20, 4);
 
 -- --------------------------------------------------------
 
@@ -70,6 +90,12 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `tok
 --
 
 --
+-- Indexes for table `hotel`
+--
+ALTER TABLE `hotel`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `sobe`
 --
 ALTER TABLE `sobe`
@@ -86,10 +112,15 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `hotel`
+--
+ALTER TABLE `hotel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `sobe`
 --
 ALTER TABLE `sobe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `users`
 --
